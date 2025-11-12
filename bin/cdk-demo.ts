90ff84049d5c36ff1c70c6969eb231a3168eaadf
acvishal <vishal.gupta@appfire.com>
2025-11-12 11:35:40 +0530

    feat: adding github workflow

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -3,6 +3,8 @@ import { ApiStack } from '../lib/stacks/api-stack';
 
 
 const app = new cdk.App();
+
+console.log("{ account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.AWS_REGION }",{ account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.AWS_REGION })
 new ApiStack(app, 'ApiStack', {
-  env: { account: '339713054130', region: 'ap-south-1' },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.AWS_REGION },
 });
